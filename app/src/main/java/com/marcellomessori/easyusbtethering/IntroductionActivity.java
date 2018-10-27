8518d89b858cac46d05c98ac422b124314035163
Sakshum Sharma <sakssharma@expedia.com>
2018-10-28 01:57:32 +0530

    Added Ad but app not working

--- app/src/main/java/com/marcellomessori/easyusbtethering/IntroductionActivity.java
+++ app/src/main/java/com/marcellomessori/easyusbtethering/IntroductionActivity.java
@@ -3,15 +3,25 @@ package com.marcellomessori.easyusbtethering;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 
 public class IntroductionActivity extends Activity {
 
+
+    private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_intro);
+        MobileAds.initialize(this,
+                "ca-app-pub-2413724092372090~4639103575");
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     public void closeActivity(View view) {
